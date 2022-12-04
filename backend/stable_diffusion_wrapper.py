45380e629759ca4af6e297cfa906dc469a6fcb0a
Janus Manlapaz <alphaomega030894@gmail.com>
2022-12-04 07:59:02 -0500

    breh

--- backend/stable_diffusion_wrapper.py
+++ backend/stable_diffusion_wrapper.py
@@ -10,10 +10,6 @@ class StableDiffusionWrapper:
         # print(f"--> Using {num_cores} cores")
         # torch.set_num_threads(num_cores)
         # Set the device to use the first GPU
-        torch.cuda.set_device(0)
-
-        # Enable memory growth on the selected GPU
-        torch.cuda.set_memory_growth(True)
         pipe = DiffusionPipeline.from_pretrained(
             repo_id, revision="fp16",
             torch_dtype=torch.float16
@@ -25,6 +21,15 @@ class StableDiffusionWrapper:
 
             
     def generate_images(self, text_prompt: str, num_images: int):
+        torch.cuda.set_device(0)
+
+        # Get the current and maximum amount of memory allocated on the GPU
+        memory_allocated = torch.cuda.memory_allocated()
+        max_memory_allocated = torch.cuda.max_memory_allocated()
+
+        # Manually free some memory if the maximum memory allocated is greater than the current memory allocated
+        if max_memory_allocated > memory_allocated:
+            torch.cuda.empty_cache()
         prompt = [text_prompt] * num_images
-        images = self.pipe(prompt, num_inference_steps=10).images
+        images = self.pipe(prompt, num_inference_steps=num_images).images
         return images
