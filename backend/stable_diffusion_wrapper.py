cf6162db5f6f27fa3eaf626734d6ccd6ba50abd6
Janus Manlapaz <alphaomega030894@gmail.com>
2022-12-04 07:22:12 -0500

    Update stable_diffusion_wrapper.py

--- backend/stable_diffusion_wrapper.py
+++ backend/stable_diffusion_wrapper.py
@@ -1,9 +1,14 @@
 from diffusers import DiffusionPipeline, DPMSolverMultistepScheduler
 import torch
+import os
 
 class StableDiffusionWrapper:
     def __init__(self) -> None:
         repo_id = "stabilityai/stable-diffusion-2-base"
+        torch.set_num_threads(8)
+        num_cores = os.cpu_count()
+        print(f"--> Using {num_cores} cores")
+        torch.set_num_threads(num_cores)
         pipe = DiffusionPipeline.from_pretrained(
             repo_id, revision="fp16",
             torch_dtype=torch.float16
