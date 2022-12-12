9f31b868004e26e2d7260a3ed345a74358fa8ac4
Janus Manlapaz <alphaomega030894@gmail.com>
2022-12-12 07:25:47 -0500

    Update stable_diffusion_wrapper.py

--- backend/stable_diffusion_wrapper.py
+++ backend/stable_diffusion_wrapper.py
@@ -1,18 +1,18 @@
-from diffusers import DiffusionPipeline, DPMSolverMultistepScheduler
+from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler
 import torch
 import os
 
 class StableDiffusionWrapper:
     def __init__(self) -> None:
-        repo_id = "stabilityai/stable-diffusion-2-base"
+        repo_id = "hakurei/waifu-diffusion"
         # torch.set_num_threads(8)
         # num_cores = os.cpu_count()
         # print(f"--> Using {num_cores} cores")
         # torch.set_num_threads(num_cores)
         # Set the device to use the first GPU
-        pipe = DiffusionPipeline.from_pretrained(
+        pipe = StableDiffusionPipeline.from_pretrained(
             repo_id, revision="fp16",
-            torch_dtype=torch.float16
+            torch_dtype=torch.float32
         )
 
         pipe.scheduler = DPMSolverMultistepScheduler.from_config(
@@ -21,15 +21,15 @@ class StableDiffusionWrapper:
 
             
     def generate_images(self, text_prompt: str, num_images: int):
-        torch.cuda.set_device(0)
+        # torch.cuda.set_device(0)
 
-        # Get the current and maximum amount of memory allocated on the GPU
-        memory_allocated = torch.cuda.memory_allocated()
-        max_memory_allocated = torch.cuda.max_memory_allocated()
+        # # Get the current and maximum amount of memory allocated on the GPU
+        # memory_allocated = torch.cuda.memory_allocated()
+        # max_memory_allocated = torch.cuda.max_memory_allocated()
 
-        # Manually free some memory if the maximum memory allocated is greater than the current memory allocated
-        if max_memory_allocated > memory_allocated:
-            torch.cuda.empty_cache()
+        # # Manually free some memory if the maximum memory allocated is greater than the current memory allocated
+        # if max_memory_allocated > memory_allocated:
+        #     torch.cuda.empty_cache()
         prompt = [text_prompt] * num_images
         images = self.pipe(prompt, num_inference_steps=num_images).images
         return images
